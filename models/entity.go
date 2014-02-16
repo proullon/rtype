bc59283a2da0ff1b371cafd9550c296fbc6772d2
Pierre <pierre.roullon@gmail.com>
2014-02-16 13:19:42 -0500

    Improve entity interface

--- models/entity.go
+++ models/entity.go
@@ -1,5 +1,45 @@
 package models
 
+import (
+    "github.com/astaxie/beego"
+)
+
 type Entity interface {
-    Live(entityChan chan Event)
+    Id() int
+    Live(eventChannel chan Event, collisionChannel chan Entity)
+    Kill()
+    PosX() int
+    PosY() int
+}
+
+type baseEntity struct {
+    id   int
+    x    int
+    y    int
+    stop chan bool
+}
+
+func (b *baseEntity) Id() int {
+    return b.id
+}
+
+func (b *baseEntity) PosX() int {
+    return b.x
+}
+
+func (b *baseEntity) PosY() int {
+    return b.y
+}
+
+func (b *baseEntity) Kill() {
+
+    if b.x > 0 {
+        beego.Info("baseEntity.Kill id=", b.id)
+    }
+
+    b.stop <- true
+
+    if b.x > 0 {
+        beego.Info("baseEntity.Kill id=", b.id, "DONE")
+    }
 }
